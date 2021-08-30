e8ee4f19f09956df72b3c8bc43499f20abb4f4bd
Aakash <123aakashsharma@gmail.com>
2021-08-30 17:51:07 +0530

    add dark theme support

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,41 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import { useCallback, useEffect, useState } from "react"
+import { Helmet } from "react-helmet"
 
+
+const useTheme = () => {
+  const [theme, setTheme] = useState()
+  const updateTheme = useCallback((theme) => {
+    setTheme(theme)
+    try {
+      localStorage.setItem("theme", theme)
+    } catch (e) {
+    }
+  }, [])
+
+  useEffect(() => {
+    // useEffect don't run on server, but still added checks
+    let savedTheme
+    try {
+      savedTheme = localStorage.getItem("theme")
+    } catch (err) {
+    }
+    const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)")
+    prefersDarkScheme.addEventListener("change", (e) => {
+      const calculatedTheme = e.matches ? "dark" : "light"
+      updateTheme(calculatedTheme)
+    })
+    updateTheme(savedTheme || (prefersDarkScheme.matches ? "dark" : "light"))
+
+  }, [updateTheme])
+  return theme
+}
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
   let header
+  const theme = useTheme()
 
   if (isRootPath) {
     header = (
@@ -22,6 +53,9 @@ const Layout = ({ location, title, children }) => {
 
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
+      <Helmet bodyAttributes={{
+        class: theme
+      }} />
       <header className="global-header">{header}</header>
       <main>{children}</main>
       <footer>
